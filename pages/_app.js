6d50dbf7c102f2b282582d770b0b280ebe1d53f2
Ryan <rmthomas1998@gmail.com>
2022-07-29 21:28:30 -0400

    analytics

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,8 @@ import { useEffect, useState } from "react";
 import { Nav } from "../components/nav/nav";
 import "../styles/globals.css";
 import { ThemeContext } from "../components/themeContext";
+import Head from "next/header";
+import Script from "next/script";
 
 const MyApp = ({ Component, pageProps }) => {
   const [theme, setTheme] = useState("dark");
@@ -21,13 +23,32 @@ const MyApp = ({ Component, pageProps }) => {
   }, []);
 
   return (
-    <GeistProvider themeType={theme}>
-      <CssBaseline />
-      <ThemeContext.Provider value={{ theme, setTheme }}>
-        <Nav theme={theme} setTheme={setTheme} />
-        <Component {...pageProps} />
-      </ThemeContext.Provider>
-    </GeistProvider>
+    <>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1"
+        />
+      </Head>
+      <Script
+        id="my-script"
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-1WCQEG8J7M`}
+      ></Script>
+      <Script strategy="lazyOnload" id="my-script-2">
+        {`window.dataLayer = window.dataLayer || [];
+            function gtag() {dataLayer.push(arguments);}
+            gtag("js", new Date());
+            gtag("config", "G-1WCQEG8J7M");`}
+      </Script>
+      <GeistProvider themeType={theme}>
+        <CssBaseline />
+        <ThemeContext.Provider value={{ theme, setTheme }}>
+          <Nav theme={theme} setTheme={setTheme} />
+          <Component {...pageProps} />
+        </ThemeContext.Provider>
+      </GeistProvider>
+    </>
   );
 };
 
